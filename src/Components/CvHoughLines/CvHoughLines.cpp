7e0e1ba4b413b4e2ceca49fbf6595736d12afa99
Anna Wujek <aniawujek@gmail.com>
2014-12-02 18:02:18 +0100

    znaleziono kartke

--- src/Components/CvHoughLines/CvHoughLines.cpp
+++ src/Components/CvHoughLines/CvHoughLines.cpp
@@ -13,6 +13,7 @@
 
 #include "Types/DrawableContainer.hpp"
 #include "Types/Line.hpp"
+#include <math.h>
 
 namespace Processors {
 namespace CvHoughLines {
@@ -77,6 +78,54 @@ bool CvHoughLines_Processor::onStart()
 	return true;
 }
 
+bool sameLines(Vec4i line1, Vec4i line2) {
+    float A1 = float(line1[3] - line1[1]) / float(line1[2] - line1[0]);
+    float b1 = line1[1] - A1 * line1[0];
+    float A2 = float(line2[3] - line2[1]) / float(line2[2] - line2[0]);
+    float b2 = line2[1] - A2 * line2[0];
+
+    float Yaxis1 = b1;
+    float Xaxis1 = -b1 / A1;
+
+    float Yaxis2 = b2;
+    float Xaxis2 = -b2 / A2;
+
+
+
+    float angle1 = atan2(float(line1[3] - line1[1]) , float(line1[2] - line1[0]));
+    float angle2 = atan2(float(line2[3] - line2[1]) , float(line2[2] - line2[0]));
+
+    float diffA = abs(angle1 - angle2)*180.0 / M_PI;
+
+    cout<<diffA<<"\t"<<A2<<"\t"<<b1<<"\t"<<b2<<endl;
+    if(diffA < 5.0 && (abs(Yaxis1 - Yaxis2) < 100 || abs(Xaxis1 - Xaxis2) < 100)) {
+        return true;
+    }
+    return false;
+}
+
+vector<Vec4i> checkDuplicate(vector<Vec4i> & lines) {
+    vector<Vec4i> newLines;
+
+    for( size_t i = 0; i < lines.size(); i++ )
+	{
+        bool diff = true;
+		for( size_t j = i+1; j < lines.size(); j++ )
+        {
+            if(sameLines(lines[i], lines[j])) {
+                diff = false;
+                break;
+            }
+        }
+        if(diff == true) {
+
+
+            newLines.push_back(lines[i]);
+        }
+	}
+	return newLines;
+}
+
 void CvHoughLines_Processor::onNewImage()
 {
 	if (in_img.empty()) {
@@ -95,29 +144,10 @@ void CvHoughLines_Processor::onNewImage()
 	Types::DrawableContainer c;
 	vector<Vec4i> lines;
 	cv::HoughLinesP( image, lines, 1, CV_PI/180, threshold, minLineLength, maxLineGap);
-
-	while(lines.size() != 4){
-        if(lines.size() > 4) {
-            threshold = threshold + 1;
-        }
-        else {
-            threshold = threshold - 1;
-        }
-        lines.clear();
-        cv::HoughLinesP( image, lines, 1, CV_PI/180, threshold, minLineLength, maxLineGap);
-        std::cout<<"thresh: "<<threshold<<std::endl;
-	}
+    lines = checkDuplicate(lines);
 
 	CLOG(LDEBUG) << "Found " << lines.size() << " lines";
-	// Needed for visualization only
-    for (int i = 0; i < lines.size(); i++)
-    {
-        cv::Vec4i v = lines[i];
-        lines[i][0] = 0;
-        lines[i][1] = ((float)v[1] - v[3]) / (v[0] - v[2]) * -v[0] + v[1];
-        lines[i][2] = image.cols;
-        lines[i][3] = ((float)v[1] - v[3]) / (v[0] - v[2]) * (image.cols - v[2]) + v[3];
-    }
+
 	for( size_t i = 0; i < lines.size(); i++ )
 	{
 		c.add(new Types::Line(cv::Point(lines[i][0], lines[i][1]), cv::Point(lines[i][2], lines[i][3])));
