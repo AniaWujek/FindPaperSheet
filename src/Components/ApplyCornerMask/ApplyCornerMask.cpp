11a75f8dacd99224e059d8bd2a89dce37ff8804a
Anna Wujek <aniawujek@gmail.com>
2015-02-24 17:22:10 +0100

    dziala znajdowanie kartki, jest maska do znajdowania krawedzi.

--- src/Components/ApplyCornerMask/ApplyCornerMask.cpp
+++ src/Components/ApplyCornerMask/ApplyCornerMask.cpp
@@ -55,8 +55,34 @@ bool ApplyCornerMask::onStart() {
 
 void ApplyCornerMask::applyMask() {
     cv::Mat img = in_img.read().clone();
+    std::vector<cv::Point2f> corners = in_corners.read();
 
-    out_img.write(img);
+    if(corners.size() == 4) {
+        cv::Mat mask(img.size(), CV_8UC1, cv::Scalar::all(0));
+
+        cv::Point points[1][4];
+
+        points[0][0] = cv::Point(corners[0].x, corners[0].y);
+        points[0][1] = cv::Point(corners[1].x, corners[1].y);
+        points[0][2] = cv::Point(corners[3].x, corners[3].y);
+        points[0][3] = cv::Point(corners[2].x, corners[2].y);
+
+
+        const cv::Point* ppt[1] = { points[0] };
+        int npt[] = { 4 };
+
+        cv::fillPoly(mask, ppt, npt, 1, cv::Scalar(255,255,255), 8);
+        cv::Mat new_img;
+        img.copyTo(new_img, mask);
+
+        out_img.write(new_img);
+    }
+
+
+
+
+
+
 }
 
 
