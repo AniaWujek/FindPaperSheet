f5ed356f810d521a1a36bd8254a6b9727e83c2e2
Anna Wujek <aniawujek@gmail.com>
2015-03-23 18:50:02 +0100

    better skeleton, task do krawedzi

--- src/Components/Skeleton/Skeleton.cpp
+++ src/Components/Skeleton/Skeleton.cpp
@@ -62,27 +62,36 @@ void Skeleton::makeSkeleton() {
 
     cv::Mat img = in_img.read().clone();
 
-    cv::threshold(img, img, 127, 255, cv::THRESH_BINARY);
-    cv::cvtColor(img,img,CV_RGB2GRAY);
+    //cv::threshold(img, img, 127, 255, cv::THRESH_BINARY);
+    //cv::cvtColor(img,img,CV_RGB2GRAY);
     cv::Mat skel(img.size(), CV_8UC1, cv::Scalar(0));
     cv::Mat temp;
     cv::Mat eroded;
     cv::bitwise_not ( img, img );
 
-    cv::Mat element = cv::getStructuringElement(cv::MORPH_CROSS, cv::Size(2, 2));
+    cv::Mat element = cv::getStructuringElement(cv::MORPH_CROSS, cv::Size(3, 3));
 
     bool done;
     for(int i = 0; i < iterations; ++i)
     {
-      cv::erode(img, eroded, element);
+      /*cv::erode(img, eroded, element);
       cv::dilate(eroded, temp, element); // temp = open(img)
       cv::subtract(img, temp, temp);
       cv::bitwise_or(skel, temp, skel);
-      eroded.copyTo(img);
+      eroded.copyTo(img);*/
+      cv::morphologyEx(img, temp, cv::MORPH_OPEN, element);
+      cv::bitwise_not(temp, temp);
+      cv::bitwise_and(img, temp, temp);
+      cv::bitwise_or(skel, temp, skel);
+      cv::erode(img, img, element);
+
+      double max;
+      cv::minMaxLoc(img, 0, &max);
+      done = (max == 0);
     }
 
 
-    out_img.write(img);
+    out_img.write(skel);
 
 
 }
